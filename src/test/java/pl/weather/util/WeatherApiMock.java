844b286a115affe40b3f84588a7f240aaa962c5c
Karetson <robert.kareta@wp.pl>
2021-07-14 17:13:57 +0200

    ref

--- src/test/java/pl/weather/util/WeatherApiMock.java
+++ src/test/java/pl/weather/util/WeatherApiMock.java
@@ -1,14 +1,17 @@
 package pl.weather.util;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @Component
 public class WeatherApiMock {
-
     private final WireMockServer wireMockServer = new WireMockServer(9000);
+
+    @Value("${service.open-weather-map.api-key}")
+    String appkey;
 
     public void setUp() {
         wireMockServer.start();
@@ -19,7 +22,9 @@ public class WeatherApiMock {
     }
 
     public void mock(String cityName, int statusCode, String payload ) {
-        wireMockServer.stubFor(get(urlPathMatching("/data/2.5/weather?q=" + cityName))
+        String units = "metric";
+
+        wireMockServer.stubFor(get(urlEqualTo("/data/2.5/weather?q=" + cityName + "&appkey=" + appkey + "&units=" + units))
                 .willReturn(aResponse()
                         .withStatus(statusCode)
                         .withHeader("Content-Type", "application/json")
