a03aaaeab29e02ca8536ca57b55ea47713eef965
Karetson <robert.kareta@wp.pl>
2021-07-14 16:57:17 +0200

    refresh

--- src/test/java/pl/weather/WeatherApplicationIntegrationTest.java
+++ src/test/java/pl/weather/WeatherApplicationIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.StreamUtils;
 import pl.weather.util.WeatherApiMock;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles(profiles = "test")
 class WeatherApplicationIntegrationTest {
 
     @Autowired
